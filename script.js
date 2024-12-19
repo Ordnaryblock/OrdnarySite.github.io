3a5820617d7f6d7cf6b6d62ed7ffaf691080ac2d
Ordnary <137104340+Ordnaryblock@users.noreply.github.com>
2024-12-19 17:37:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-window.onscroll = function() {
-    shrinkHeader();
-};
 
-function shrinkHeader() {
-    const header = document.getElementById('header');
-    if (window.scrollY > 50) {
-        header.classList.add('shrink');
-    } else {
-        header.classList.remove('shrink');
-    }
-}
